0b715a688ac2b11e45a279ec1844f765b6b0fd43
Juliano Pinheiro <julianopinheiro.adm@gmail.com>
2018-09-11 13:52:44 -0300

    Iniciando projeto - autenticacao

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const morgan = require('morgan');
 const mysql = require('mysql');
 const myConnection = require('express-myconnection');
-
+const bodyParser = require('body-parser');
 const app = express();
 
 //settings
@@ -11,18 +11,50 @@ app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 //middlewares
 app.use(morgan('dev'));
-app.use(myConnection(mysql, {
-    host: 'localhost',
-    user: 'root',
-    password: 'abc123',
-    port: 3306,
-    database: 'projweb'
-}, 'single'));
+// app.use(myConnection(mysql, {
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'abc123',
+//     port: 3306,
+//     database: 'projweb'
+// }, 'single'));
 
 //routes
 
 app.listen(app.get('port'), () => {
     console.log('Servidor OnLine na porta 3000');
 });
+
+app.get('/registro', (req, res) => {
+    res.render('login');
+})
+
+app.post('/cadastro', (req, res) => {
+    const usuario = req.body;
+
+    const conn = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'abc123',
+        port: 3306,
+        database: 'projweb'
+    })
+
+    conn.query('insert into users set ?', usuario, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Deu certo pô!');
+
+        res.send("ok");
+    })
+
+})
